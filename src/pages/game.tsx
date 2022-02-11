eb02b62cc4d7da089f99886088dd56f6ce11083f
yanggak12 <yanggak12@naver.com>
2022-02-11 19:10:44 +0900

    feat : game page

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,11 +1,37 @@
 import type { NextPage } from "next";
 import Seo from "../components/Seo";
+import { useState } from "react";
+import ColorBox from "../components/ColorBox";
+import getBoxCnt from "../container/getBoxCnt";
+import getBoxRange from "../container/getBoxRange";
+import getTargetIndex from "../container/getTargetIndex";
 
 const Game: NextPage = () => {
+  const [stage, setStage] = useState(1);
+  const boxCnt = getBoxCnt(stage);
+  const boxRange = getBoxRange(boxCnt);
+  const targetIndex = getTargetIndex(boxCnt);
+  console.log(targetIndex);
   return (
     <div>
       <Seo title="Game" />
       <h1>Game</h1>
+      <div className="boxContainer">
+        {boxRange.map((val, idx) => (
+          <div key={idx} className="box">
+            <ColorBox color="#fff" isTarget={val === targetIndex} />
+          </div>
+        ))}
+      </div>
+
+      <style jsx>{`
+        .boxContainer {
+          display: flex;
+        }
+        .box {
+          display: flex;
+        }
+      `}</style>
     </div>
   );
 };
