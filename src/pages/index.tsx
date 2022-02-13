5f5403c9cb08583922875efcbbcd9df77f142e49
yanggak12 <yanggak12@naver.com>
2022-02-13 14:48:11 +0900

    feat : index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,59 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useState } from "react";
+import { useInterval } from "usehooks-ts";
 import Seo from "../components/Seo";
+import getRandomColor from "../container/getRandomColor";
 
 const Home: NextPage = () => {
+  const [titleColor, setTitleColor] = useState("#000");
+  useInterval(() => {
+    setTitleColor(getRandomColor() + "0.8)");
+  }, 2000);
   return (
     <div>
       <Seo title="Home" />
-      <Link href={{ pathname: "/game", query: { prev: "/" } }} as="game">
-        <a>
-          <h1>Game</h1>
-        </a>
-      </Link>
-      <h1>Hello World</h1>
+      <section>
+        <h1 className="title">
+          <Image src={"/logo.png"} width={100} height={100} alt={"logo"} />
+          Selecolor
+        </h1>
+        <Link href={{ pathname: "/game", query: { prev: "/" } }} as="game">
+          <a className="start">
+            <h1>START</h1>
+          </a>
+        </Link>
+      </section>
+      <style jsx>{`
+        .title {
+          margin-top: 10vh;
+          font-size: 48px;
+          color: #fff;
+          width: 7em;
+          height: 7em;
+          background-color: ${titleColor};
+          border-radius: 24px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .start {
+          text-decoration: none;
+          color: #000;
+        }
+        .start > h1 {
+          border: 3px solid #000;
+          border-radius: 12px;
+          font-size: 24px;
+          padding: 5px;
+        }
+        .start > h1:hover {
+          opacity: 0.7;
+          transform: translateY(0.05em);
+        }
+      `}</style>
     </div>
   );
 };
