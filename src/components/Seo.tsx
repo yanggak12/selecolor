9b3116942469d2d2fa5a21d3e1204d6b52cf166a
yanggak12 <yanggak12@naver.com>
2022-02-13 14:47:47 +0900

    feat : github link

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
   title: string;
@@ -6,9 +8,18 @@ interface Props {
 
 const Seo: React.FC<Props> = ({ title }) => {
   return (
-    <Head>
-      <title>{title} | Selecolor</title>
-    </Head>
+    <>
+      <Head>
+        <title>{title} | Selecolor</title>
+      </Head>
+      <div className="my">
+        <Link href={"https://github.com/yanggak12/selecolor"}>
+          <a>
+            <Image src={"/github.svg"} width={20} height={20} alt={"github"} />
+          </a>
+        </Link>
+      </div>
+    </>
   );
 };
 
