9c27635510a523d648cd80a19a6e1aa3fafd08ee
yanggak12 <yanggak12@naver.com>
2022-02-18 15:25:59 +0900

    chore : og:image

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -14,7 +14,14 @@ const Seo: React.FC<Props> = ({ title }) => {
           property="og:description"
           content="Select a diffrent color box.\nVery simple game!"
         />
-        <meta property="og:image" content="/logo.png" />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/46489446/154629430-eb2a85b1-d54c-4ecb-923d-d8934523ddc3.png"
+        />
+        <meta
+          name="google-site-verification"
+          content="Bd5ZNg7If_uzggm0neoD-rgWovmO6rGC_j9qYSFjKd4"
+        />
       </Head>
     </>
   );
