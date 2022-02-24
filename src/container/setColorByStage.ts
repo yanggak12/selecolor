9f81fac43eccdc28a24b429cf586b87bee5a9b53
yanggak12 <yanggak12@naver.com>
2022-02-24 20:22:44 +0900

    fix : return arr

--- src/container/setColorByStage.ts
+++ src/container/setColorByStage.ts
@@ -1,11 +1,12 @@
 const setColorByStage = (color: string, stage: number) => {
   const numberByStage = 100 / stage;
-  const rgbArr = color.split(",");
-  rgbArr.map((c) =>
-    Number(c) > numberByStage
-      ? Number(c) - numberByStage
-      : Number(c) + numberByStage
-  );
+  const rgbArr = color
+    .split(",")
+    .map((c) =>
+      Number(c) > numberByStage
+        ? Number(c) - numberByStage
+        : Number(c) + numberByStage
+    );
   return `${rgbArr[0] + "," + rgbArr[1] + "," + rgbArr[2]}`;
 };
 
